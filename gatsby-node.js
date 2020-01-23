03910e2de080c84afe1fb655e202a125a5a3c2a1
oa19 <50533202+oa19@users.noreply.github.com>
2020-01-23 22:40:08 +0000

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,6 +40,8 @@ exports.createPages = ({ graphql, actions }) => {
     )
   })
 }
+const Promise = require('bluebird')
+const path = require('path')
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   
